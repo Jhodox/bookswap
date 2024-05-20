9e2e4ace8c041cca047a62a3053038c20bea972b
IvanHernandez <diego.hernandez5523@alumnos.udg.mx>
2024-05-20 01:15:01 -0600

    Modal para contacto en footer
    Falta la lógica para que funcione sin afectar las clases de lo demas

--- main-footer.php
+++ main-footer.php
@@ -1,113 +1,163 @@
-<footer class="ps-footer">
 
-        <div class="container">
+    <footer class="ps-footer">
+        
 
-            <div class="ps-footer__widgets">
+            <div class="container">
 
-            	<!--=====================================
-				Contact us
-				======================================-->  
+                <div class="ps-footer__widgets">
 
-                <aside class="widget widget_footer widget_contact-us">
+                    <!--=====================================
+                    Contact us
+                    ======================================-->  
 
-                    <h4 class="widget-title">Contactanos!</h4>
+                    <aside class="widget widget_footer widget_contact-us">
 
-                    <div class="widget_content">
+                        <h4 class="widget-title">Contactanos!</h4>
 
-                        <p>Llamanos 24/7</p>
-                        <h3>1800 97 97 69</h3>
-                        <p>CUCEI, Jalisco, México <br>
-                        	<a href="mailto:contact@marketplace.com">contact@marketplace.com</a>
-                    	</p>
+                        <div class="widget_content">
 
-                        <ul class="ps-list--social">
-                            <li><a class="facebook" href="#"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a class="twitter" href="#"><i class="fab fa-twitter"></i></a></li>
-                            <li><a class="google-plus" href="#"><i class="fab fa-youtube"></i></a></li>
-                            <li><a class="instagram" href="#"><i class="fab fa-instagram"></i></a></li>
+                            <p>Llamanos 24/7</p>
+                            <h3>1800 97 97 69</h3>
+                            <p>CUCEI, Jalisco, México <br>
+                                <a href="mailto:contact@marketplace.com">contact@marketplace.com</a>
+                            </p>
+
+                            <ul class="ps-list--social">
+                                <li><a class="facebook" href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a class="twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                                <li><a class="google-plus" href="#"><i class="fab fa-youtube"></i></a></li>
+                                <li><a class="instagram" href="#"><i class="fab fa-instagram"></i></a></li>
+                            </ul>
+
+                        </div>
+
+                    </aside>
+
+                    <!--=====================================
+                    Quick Links
+                    ======================================-->  
+
+                    <aside class="widget widget_footer">
+
+                        <h4 class="widget-title">Links Rápidos</h4>
+
+                        <ul class="ps-list--link">
+
+                            <li><a href="#inicio">Inicio</a></li>
+
+                            <li><a href="#mas-buscados">Los Más Buscados</a></li>
+
+                            <li><a href="#mas-intercambiados">Con Mayor Cantidad de Intercambios</a></li>
+
+                            <li><a href="#recientes">Añadidos Recientemente</a></li>
+
                         </ul>
 
-                    </div>
+                    </aside>
 
-                </aside>
+                    <!--=====================================
+                    Company
+                    ======================================-->  
 
-                <!--=====================================
-				Quick Links
-				======================================-->  
+                    <aside class="widget widget_footer">
 
-                <aside class="widget widget_footer">
+                        <h4 class="widget-title">Equipo de desarrollo</h4>
 
-                    <h4 class="widget-title">Links Rápidos</h4>
+                        <ul class="ps-list--link">
 
-                    <ul class="ps-list--link">
+                            <li><a href="about-us">Sobre nosotros</a></li>
 
-                        <li><a href="#inicio">Inicio</a></li>
+                            <li><a href="#AbrirModalContacto" data-bs-toggle="modal" data-bs-target="#modalContacto" data-bs-whatever="@mdo">Contacto</a></li>
 
-                        <li><a href="#mas-buscados">Los Más Buscados</a></li>
+                        </ul>
 
-                        <li><a href="#mas-intercambiados">Con Mayor Cantidad de Intercambios</a></li>
+                    </aside>
 
-                        <li><a href="#recientes">Añadidos Recientemente</a></li>
+                    <!--=====================================
+                    Bussiness
+                    ======================================-->  
 
-                    </ul>
+                    <aside class="widget widget_footer">
 
-                </aside>
+                        <h4 class="widget-title">Cuenta</h4>
 
-                <!--=====================================
-				Company
-				======================================-->  
+                        <ul class="ps-list--link">
 
-                <aside class="widget widget_footer">
+                            <li><a href="wishlist">Wishlist</a></li>
 
-                    <h4 class="widget-title">Equipo de desarrollo</h4>
+                            <li><a href="perfil">Mi Cuenta</a></li>
+                            
+                            <li><a href="javascript:cerrar_sesion()">Cerrar sesión</a></li>
+                            
+                        </ul>
 
-                    <ul class="ps-list--link">
+                    </aside>
 
-                        <li><a href="about-us">Sobre nosotros</a></li>
+                </div>
 
-                        <li><a href="#">Unete</a></li>
-
-                        <li><a href="contact-us">Contacto</a></li>
-
-                    </ul>
-
-                </aside>
-
-                <!--=====================================
-				Bussiness
-				======================================-->  
-
-                <aside class="widget widget_footer">
-
-                    <h4 class="widget-title">Cuenta</h4>
-
-                    <ul class="ps-list--link">
-
-                        <li><a href="wishlist">Wishlist</a></li>
-
-                        <li><a href="perfil">Mi Cuenta</a></li>
-                        
-                        <li><a href="javascript:cerrar_sesion()">Cerrar sesión</a></li>
-                        
-                    </ul>
-
-                </aside>
-
-            </div>
-
-          	
-
-            <!--=====================================
-			CopyRight - Payment method Footer
-			======================================-->  
-
-            <div class="ps-footer__copyright">
-
-                <p>© <?php echo date("Y"); ?> BookSwap. Todos los derechos reservados. </p>
-
-
-            </div>
-
-        </div>
-
+   
     </footer>
+
+
+<!--=====================================
+#region modal contacto
+======================================-->  
+<!-- <body>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="path/to/funciones.js"></script>
+
+    <div class="modal fade" id="modalContacto" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title title" id="modalContactoLabel">Nuestro Contacto:</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="text-justify">
+                        <ul>
+                            <li>
+                                <strong>Llamanos 24/7</strong><br>
+                                1800 97 97 69
+                            </li><br>
+                            <li>
+                                <strong>Nuestro correo:</strong><br>
+                                contact@marketplace.com
+                            </li><br>
+                            <li>
+                                <strong>Nos ubicamos en:</strong><br>
+                                CUCEI, Jalisco, México
+                            </li><br>
+                            <li>
+                                <strong>Nuestras redes son:</strong><br>
+                                
+                            </li><br>
+                        </ul>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <p class="text-justify">
+                        Puedes contactarnos vía telefónica, o por correo. Nuestra dirección esta disponible por si llega a haber alguna consulta física.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/main.js"></script>
+
+    <script src="assets/js/jquery-2.2.4.min.js"></script>
+    <script src="assets/js/slick.min.js"></script>
+    <script src="assets/js/jquery-ui.js"></script>
+    <script src="assets/js/jquery.nice-select.js"></script>
+    <script src="assets/js/scripts.js"></script>
+    <script src="assets/js/funciones.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="assets/plugins/sweetalert/sweetalert.min.js"></script> 
+    <script src="assets/plugins/sweetalert/jquery.sweet-alert.custom.js"></script>
+
+</body> -->
