cddc513c266aef66107d67ca499d778503e9542d
IvanHernandez <diego.hernandez5523@alumnos.udg.mx>
2024-05-21 17:12:53 -0600

    Cambio de imagen de libro y pagina de admins V2

--- index.php
+++ index.php
@@ -34,8 +34,8 @@ session_start();
 	<!-- linear icons -->
 	<link rel="stylesheet" href="css/plugins/linearIcons.css">
 
-	<!-- Bootstrap 4
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"> -->
+	<!-- Estilo Admin -->
+    <link rel="stylesheet" href="assets/css/admin-style.css">
 	
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
