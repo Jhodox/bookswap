99ccbd1bbf633cafdd2327739712c804b12e3b1e
Jhodox <cris.gamers50@gmail.com>
2024-05-21 18:14:44 -0600

    Estilos de libros en paginas index, usuario y libro

--- libro.php
+++ libro.php
@@ -432,7 +432,8 @@ session_start();
 
                 ?>
 
-                    <aside class="widget widget_same-brand">
+                    <aside class="widget widget_same-brand" style="box-shadow: 9px 9px 8px #A3B1C6,
+    -9px -9px 16px #FFF;">
 
                         <h3>Por el mismo usuario</h3>
 
@@ -459,7 +460,7 @@ session_start();
                                 $url_producto = preg_replace('/[^a-zA-Z0-9\s-]/', '', $url_producto);
                                 ?>
 
-                                <div class="ps-product">
+                                <div class="ps-product ps-product--simple">
 
                                     <div class="ps-product__thumbnail">
 
